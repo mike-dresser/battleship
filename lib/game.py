14cfd3c74c4fdb5a1377d008d9fb9a148eb44056
brentnettles <brentnettles@gmail.com>
2024-03-22 12:27:10 -0400

    set cpu ships + player fire shot

--- lib/game.py
+++ lib/game.py
@@ -6,6 +6,7 @@ class Game:
 
     def __init__(self):
         self.player_grid = Grid() 
+        self.cpu_grid = Grid() 
         # eventually we will have a computer grid as well
     
     def play(self):
@@ -15,8 +16,13 @@ class Game:
         print('~ ~ ~  B A T T L E S H I P  ~ ~ ~\n\n')
         print('The year is 2024. Oceans are battlefields.\n')
     
+        print("~~HARBOR~~")
         self.player_grid.display_game_board()
         self.place_ships()
+        print("~~BATTLEFIELD~~")
+        self.cpu_grid.cpu_ship_placement()
+        self.cpu_grid.display_game_board()
+        self.take_player_shot()
 
     def place_ships(self):
         print('Begin by placing your 1x1 ships on the game board.\n')
@@ -24,7 +30,7 @@ class Game:
         while placed_ships < 3:
             print(f'*** {3 - placed_ships} ships remaining ***\n')
             pos = Game.get_position_input('Position for your ship (i.e. "B2") ')
-            os.system('clear')
+            # os.system('clear')
             if self.player_grid.query_position(pos[0], pos[1]) == 'S':
                 self.player_grid.display_game_board()
                 print('\nCannot place a ship on top of another!\n')
@@ -64,5 +70,29 @@ class Game:
             accepted = True
         return result
 
+    @classmethod
+    def display_cpu(self):
+        print("You Cannot Hide")
+        return self.cpu_grid.display_game_board()
+    
+    ##------- Outline for Player Fire
+    
+    def take_player_shot(self):
+        print('Aim your cannon')
 
+        pos = self.get_position_input('Enter position to fire : ')
+        row = pos[0]
+        column = pos[1]
+        if self.cpu_grid.query_position(row, column) in ['H', 'M']:
+            print('You have already fired at this position. Try again!')
+        
+        else:
+            result = self.cpu_grid.query_position(row, column)
+            if result == 'S':
+                print('Hit!')
+                self.cpu_grid.change_grid(row, column, 'H')
+            else:
+                print('Miss!')
+                self.cpu_grid.change_grid(row, column, 'M')
+                
     
