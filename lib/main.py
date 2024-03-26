cbec8e8242f1c9b045d43e5189e0ddd609391ab9
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-03-26 11:06:47 -0400

    Move replay loop into main

--- lib/main.py
+++ lib/main.py
@@ -3,8 +3,13 @@ from curses import wrapper
 
 def main(stdscr):
     """ Main game loop. `stdscr` is curses screen object"""
-    x = Game(stdscr)
-    x.play()
+    play_again = True
+    while play_again:
+        '''for right now I have the while loop on the whole play() but I think we should figure out a more efficient way
+        Enables the "P to play again" at the end of a game'''
+
+        x = Game(stdscr)
+        play_again = x.play()
 
 # Run `wrapper` to initialize curses, pass main loop
 wrapper(main)
